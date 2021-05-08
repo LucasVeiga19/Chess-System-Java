ed5df5026111648d63087fed8b3fab90e361cc3b
LucasVeiga19 <lucas.veiga@laveiga.com.br>
2021-05-08 10:48:43 -0300

    Possible moves of a piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -28,11 +28,7 @@ public class Program {
 
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
             }
-            catch (ChessException e){
-                System.out.println(e.getMessage());
-                sc.nextLine();
-            }
-            catch (InputMismatchException e) {
+            catch (ChessException | InputMismatchException e){
                 System.out.println(e.getMessage());
                 sc.nextLine();
             }
